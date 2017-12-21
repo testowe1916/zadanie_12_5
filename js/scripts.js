6be403aa088a379524a736e3a8f6debcb8a0baf8
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-12-21 11:42:22 +0100

    mod js

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,6 @@ function createTweet(input) {
   if (!quoteAuthor.length) {
       quoteAuthor = "Unknown author";
     }
-}
 var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
   if (tweetText.length > 140) {
     getQuote();
@@ -21,6 +20,7 @@ var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
     $('.quote').text(quoteText);
     $('.author').text("Author: " + quoteAuthor);
     $('.tweet').attr('href', tweet);
+}
 }
 $(document).ready(function() {
   getQuote();
